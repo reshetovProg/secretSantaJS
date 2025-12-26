70e179c9f3b4819558cfce9acc19faf724092e72
Взрослая академия <reshetov@mail.ru>
2025-12-26 19:04:55 +0300

    localStorage

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-users = []
-currentUser = {}
-
-
 function registry(){
+    let readJson = localStorage.getItem("users")
+    let users = readJson ? JSON.parse(readJson) : [];
+
     name = document.getElementById("name").value;
     login = document.getElementById("login").value;
     password = document.getElementById("password").value;
-    users.push({"name": name, "login": login, "password": password});   
-    // window.location.replace("index.html");
+    users.push({"name": name, "login": login, "password": password});
     let json = JSON.stringify(users);
-    console.log(json);
+    localStorage.setItem("users",json);   
+    window.location.replace("index.html");
 }
 
 function showUsers(){
     boxUsers = document.getElementsByClassName("users")[0]
+    let json = localStorage.getItem("users")
+    let users = JSON.parse(json)
     users.forEach(user => {
-        let line = document.createAttribute("div")
+        let line = document.createElement("div")
         line.textContent = `${user.name} : ${user.login} : ${user.password}`
-        boxUsers.setAttributeNode(line);
+        boxUsers.appendChild(line);
     });
+}
+
+function clearUsers(){
+    localStorage.clear();
 }
