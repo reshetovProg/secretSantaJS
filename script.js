f7d634093cf7ba7c18d52be1652c5ded47dd88e3
Взрослая академия <reshetov@mail.ru>
2025-12-26 20:48:28 +0300

    login verification

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let user = JSON.parse(localStorage.getItem("currentUser"))
+let autoriseElements = document.querySelectorAll(".autorise");
+let unatoriseElements = document.querySelectorAll(".unautorise");
+console.log(autoriseElements)
+console.log(unatoriseElements)
+autoriseElements.forEach(element => {
+    element.style.display = user? "block" : "none";
+});
+unatoriseElements.forEach(element => {
+    element.style.display = user? "none" : "block";
+});
+
+if (user){
+    let userLine = document.querySelector("#user");
+    userLine.textContent = `${user.name} (${user.login})`
+}
+
+
+
+
 function registry(){
     let readJson = localStorage.getItem("users")
     let users = readJson ? JSON.parse(readJson) : [];
@@ -13,6 +33,10 @@ function registry(){
 
 function showUsers(){
     boxUsers = document.getElementsByClassName("users")[0]
+    boxUsers.replaceChildren();
+    // while (boxUsers.firstChild) { 
+    //     boxUsers.removeChild(boxUsers.firstChild); 
+    // }
     let json = localStorage.getItem("users")
     let users = JSON.parse(json)
     users.forEach(user => {
@@ -24,4 +48,31 @@ function showUsers(){
 
 function clearUsers(){
     localStorage.clear();
+    boxUsers.replaceChildren();
+    // while (boxUsers.firstChild) { 
+    //     boxUsers.removeChild(boxUsers.firstChild); 
+    // }
+}
+
+function loginUser(){
+    let json = localStorage.getItem("users");
+    let users = JSON.parse(json);
+    login = document.getElementById("login").value;
+    password = document.getElementById("password").value;
+    users?.forEach(user => {
+        if (user.login == login && user.password == password){
+            localStorage.setItem("currentUser",JSON.stringify({"name": user.name, "login": user.login}));
+            window.location.replace("index.html");
+            return;   
+        } 
+    });
+
+    let errorLine = document.querySelector(".incorrectUser");
+    errorLine.style.visibility = "visible";
+
+}
+
+function logout(){
+    localStorage.removeItem("currentUser");
+    window.location.replace("index.html");
 }
